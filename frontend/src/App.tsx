d3bfd364f7686529216d43b869587df72b6f64ad
prisyaga <papsavel@gmail.com>
2026-02-14 21:47:01 +0200

    chore: initialize frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ComponentName = () => {
+  return (
+    <div>
+      <h1 className="text-2xl text-red-400">Hello</h1>
+    </div>
+  );
+};
+
+export default ComponentName;
