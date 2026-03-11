6cc4c1c13d6a98bae2b9f83fc844861ed718c45c
prisyaga <papsavel@gmail.com>
2026-03-11 14:27:25 +0200

    feat: add custom validation error message

--- backend/src/utils/validateSchema.ts
+++ backend/src/utils/validateSchema.ts
@@ -3,11 +3,11 @@ import { ZodType } from 'zod';
 import { FastifyRequest } from 'fastify';
 import { AppError } from '../commons/types/error.js';
 
-const validateSchema = <T extends Record<string, any>>(request: FastifyRequest, schema: ZodType<T>): T => {
+const validateSchema = <T extends Record<string, any>>(request: FastifyRequest, schema: ZodType<T>, errorMessage: string): T => {
   const parseResult = schema.safeParse(request.body);
   
   if (!parseResult.success) {
-    const error = request.server.httpErrors.badRequest('Invalid request body') as AppError;
+    const error = request.server.httpErrors.badRequest(errorMessage) as AppError;
     error.details = parseResult.error;
     throw error;
   }
