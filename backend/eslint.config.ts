c5681f3c2618332a9edc0f386ebcac0813b84eca
prisyaga <papsavel@gmail.com>
2026-03-16 20:11:12 +0200

    chore: configure eslint

---
+++ backend/eslint.config.ts
@@ -0,0 +1,33 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    ignores: [
+      "dist/**",
+      "**/*.js",
+      "**/*.mjs",
+      "**/*.cjs",
+      "**/*.json",
+      "**/*.md",
+      "**/*.css",
+      "**/*.html",
+    ],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      semi: ["warn", "always"],
+    },
+  },
+];
