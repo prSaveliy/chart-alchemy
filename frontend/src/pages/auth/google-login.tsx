39edf40734d240895887ceee5f705731ceabbd87
prisyaga <papsavel@gmail.com>
2026-03-26 14:47:12 +0200

    feat: save accessToken and user picture to localStorage

--- frontend/src/pages/auth/google-login.tsx
+++ frontend/src/pages/auth/google-login.tsx
@@ -3,31 +3,45 @@ import React from 'react'
 import { Loading } from "../loading";
 
 export const GoogleLogin = () => {
-  const authorize = () => {
+  const authorize = async () => {
     const queryParams = new URLSearchParams(window.location.search)
     const code = queryParams.get('code');
     const state = queryParams.get('state');
     if (code) {
-      fetch('http://localhost:3000/oauth/google/handle-code', {
-        method: "POST",
-        credentials: 'include',
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          code,
-          state,
-        }),
-      })
-        .then(res => {
-          if (!res.ok) throw new Error("OAuth failed");
-          window.location.href = import.meta.env.VITE_API_URL;
-        })
-        .catch(console.error);
+      try {
+        const response = await fetch('http://localhost:3000/oauth/google/handle-code', {
+          method: "POST",
+          credentials: 'include',
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            code,
+            state,
+          }),
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+          console.log(response.status);
+          return;
+        }
+
+        localStorage.setItem('picture', data.picture);
+        localStorage.setItem('accessToken', data.accessToken);
+
+        // change to dashboard later
+        window.location.href = `${import.meta.env.VITE_API_URL}/new-chart`;
+      } catch (err) {
+        console.log(err);
+      }
+      
+
     }
   }
   
-  React.useEffect(authorize, []);
+  React.useEffect(() => { authorize() }, []);
   
   return (
     <Loading message="Logging in"/>
