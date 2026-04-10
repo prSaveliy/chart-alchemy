50c687a7412dbb4eeb6967d345be0041d7a28c0c
prisyaga <papsavel@gmail.com>
2026-04-10 16:01:48 +0300

    fix(header): add no-referrer policy and local fallback for avatar image

--- frontend/src/components/layout/header2.tsx
+++ frontend/src/components/layout/header2.tsx
@@ -9,6 +9,7 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Logo } from "@/components/layout/logo";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import user from "@/assets/user.png";
 
 import { ChevronDown } from "lucide-react";
 import { useNavigate } from "react-router-dom";
@@ -37,7 +38,10 @@ export const Header2 = (props: { userPicture: string | null }) => {
             <DropdownMenuTrigger>
               <button className="flex items-center justify-center w-18 h-12 rounded-2xl hover:bg-black/2 cursor-pointer">
                 <Avatar className="mr-1">
-                  <AvatarImage src={props.userPicture} alt="@shadcn" />
+                  <AvatarImage
+                    src={props.userPicture ?? user}
+                    referrerPolicy="no-referrer"
+                  />
                 </Avatar>
                 <ChevronDown className="mt-1 text-black/60 w-5 h-5" />
               </button>
