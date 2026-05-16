5c03b8cc494c8b455d94598ae5429dda1c53255f
prisyaga <papsavel@gmail.com>
2026-05-16 19:59:43 +0300

    fix: handle Heroku TLS Redis URL

--- backend/src/plugins/redis.plugin.ts
+++ backend/src/plugins/redis.plugin.ts
@@ -4,9 +4,18 @@ import { FastifyInstance, FastifyPluginAsync } from 'fastify';
 import fastifyRedis from '@fastify/redis';
 
 const redisPlugin: FastifyPluginAsync = async (fastify: FastifyInstance) => {
+  const isTlsRedis = fastify.config.REDIS_URL.startsWith('rediss://');
+
   await fastify.register(fastifyRedis, {
     url: fastify.config.REDIS_URL,
     closeClient: true,
+    ...(isTlsRedis
+      ? {
+          tls: {
+            rejectUnauthorized: false,
+          },
+        }
+      : {}),
   });
 };
 
